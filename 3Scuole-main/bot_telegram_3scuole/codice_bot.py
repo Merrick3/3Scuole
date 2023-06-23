b5028daf64c2d36d1aaba6d6d34098d44df68b70
Skipper57 <simone2kilo5@gmail.com>
2023-06-23 10:15:16 +0200

    aiuto

--- 3Scuole-main/bot_telegram_3scuole/codice_bot.py
+++ 3Scuole-main/bot_telegram_3scuole/codice_bot.py
@@ -8,10 +8,24 @@ from pprint import pprint
 import time
 import datetime
 import json
+from aiogram import *
+from aiogram.types import *
 # IMPORTANTE: inserire il token fornito dal BotFather nella seguente stringa
 with open(r"C:\Users\Simone\Documents\GitHub\3scuole fork\3Scuole\3Scuole-main\bot_telegram_3scuole\token.txt", "r") as f:
     TOKEN = f.read()
-    bot = telepot.Bot(TOKEN)
+    bot = Bot(TOKEN)
+    dp =  Dispatcher(bot)
+button0 = InlineKeyboardButton(text='Bussolengo', callback_data = '1')
+button1 = InlineKeyboardButton(text='Castelnuovo', callback_data = '2')
+button2 = InlineKeyboardButton(text='Lazise', callback_data = '3')
+button3 = InlineKeyboardButton(text='Mozzecane', callback_data = '4')
+button4 = InlineKeyboardButton(text='Pastrengo', callback_data = '5')
+button5 = InlineKeyboardButton(text='Pescantina', callback_data = '6')
+button6 = InlineKeyboardButton(text='Sommacampagna', callback_data = '7')
+button7 = InlineKeyboardButton(text='Sona', callback_data = '8')
+button8 = InlineKeyboardButton(text='Valeggio', callback_data = '9')
+button9 = InlineKeyboardButton(text='Vigasio', callback_data = '10')
+button10 = InlineKeyboardButton(text='Villafranca', callback_data = '11')
     #print("Il tuo token è ", TOKEN)
 
 async def avvia(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -41,7 +55,8 @@ async def paese(update: Update, context: ContextTypes.DEFAULT_TYPE):
         11 Vigasio)
 """
 async def paese(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    on_chat_message
+    keyboard_inline = InlineKeyboardMarkup().add(button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, button10)
+"""
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
@@ -56,42 +71,43 @@ def on_chat_message(msg):
                      [InlineKeyboardButton(text='Valeggio', callback_data='valeggio'),
                      InlineKeyboardButton(text='Vigasio', callback_data='vigasio')],
                      [InlineKeyboardButton(text='Villafranca', callback_data='villafranca')]])
-    bot.sendMessage(chat_id, 'Ciao, mi chiamo MonuVeronaBot!🤖 Ti aiuterò nella ricerca dei monumenti più interessanti dell\'ovest veronese!🧭\nScegli il comune tra l\'elenco:', reply_markup=keyboard)
+    bot.sendMessage(chat_id, 'Ciao, mi chiamo Schooldecider! Ti aiuterò nella ricerca dei monumenti più interessanti dell\'ovest veronese!\nScegli il comune tra l\'elenco:', reply_markup=keyboard)
+"""
 def on_callback_query(msg):
     query_id, chat_id, query_data = telepot.glance(msg, flavor='callback_query')
     print('Callback Query:', query_id, chat_id, query_data)
   
-    if query_data == 'bussolengo':
+    if query_data == '1':
         bot.sendMessage(chat_id, 'D\'accordo, hai scelto il comune di Bussolengo!\nOra scegli il tipo di monumento che vorresti visitare tra l\'elenco:')
     
-    elif query_data == 'castelnuovo':
+    elif query_data == '2':
         bot.sendMessage(chat_id, 'D\'accordo, hai scelto il comune di Castelnuovo!\nOra scegli il tipo di monumento che vorresti visitare tra l\'elenco:')
     
-    elif query_data == 'lazise':
+    elif query_data == '3':
         bot.sendMessage(chat_id, 'D\'accordo, hai scelto il comune di Lazise!\nOra scegli il tipo di monumento che vorresti visitare tra l\'elenco:')
  
-    elif query_data == 'mozzecane':
+    elif query_data == '4':
         bot.sendMessage(chat_id, 'D\'accordo, hai scelto il comune di Mozzecane!\nOra scegli il tipo di monumento che vorresti visitare tra l\'elenco:')
       
-    elif query_data == 'pastrengo':
+    elif query_data == '5':
         bot.sendMessage(chat_id, 'D\'accordo, hai scelto il comune di Pastrengo!\nOra scegli il tipo di monumento che vorresti visitare tra l\'elenco:')
     
-    elif query_data == 'pescantina':
+    elif query_data == '6':
         bot.sendMessage(chat_id, 'D\'accordo, hai scelto il comune di Pescantina!\nOra scegli il tipo di monumento che vorresti visitare tra l\'elenco:')
  
-    elif query_data == 'sommacampagna':
+    elif query_data == '7':
         bot.sendMessage(chat_id, 'D\'accordo, hai scelto il comune di Sommacampagna!\nOra scegli il tipo di monumento che vorresti visitare tra l\'elenco:')
   
-    elif query_data == 'sona':
+    elif query_data == '8':
         bot.sendMessage(chat_id, 'D\'accordo, hai scelto il comune di Sona!\nOra scegli il tipo di monumento che vorresti visitare tra l\'elenco:')
     
-    elif query_data == 'valeggio':
+    elif query_data == '9':
         bot.sendMessage(chat_id, 'D\'accordo, hai scelto il comune di Valeggio!\nOra scegli il tipo di monumento che vorresti visitare tra l\'elenco:')
  
-    elif query_data == 'vigasio':
+    elif query_data == '10':
         bot.sendMessage(chat_id, 'D\'accordo, hai scelto il comune di Vigasio!\nOra scegli il tipo di monumento che vorresti visitare tra l\'elenco:')
   
-    elif query_data == 'villafranca':
+    elif query_data == '11':
         bot.sendMessage(chat_id, 'D\'accordo, hai scelto il comune di Villafrancaa!\nOra scegli il tipo di monumento che vorresti visitare tra l\'elenco:')
 async def pupr(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("""Scegli tra le seguenti:
